dfbd112edf2dda3bb49035071e640e5a16eb37d9
wonyuyeong <elseks123@gmail.com>
2024-04-29 21:28:04 +0900

    나의여행 살짝수정

--- src/main/java/com/ict/jeju/wyy/controller/AjaxController4.java
+++ src/main/java/com/ict/jeju/wyy/controller/AjaxController4.java
@@ -1,18 +1,11 @@
 package com.ict.jeju.wyy.controller;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.google.gson.Gson;
 import com.ict.jeju.wyy.dao.CalendarVO4;
