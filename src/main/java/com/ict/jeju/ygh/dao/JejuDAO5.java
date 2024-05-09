38f0d40c6b0121928973d9d8c94181bf898c3110
yugh <yugh0922@gmail.com>
2024-05-09 19:32:39 +0900

    관리자 수정중

--- src/main/java/com/ict/jeju/ygh/dao/JejuDAO5.java
+++ src/main/java/com/ict/jeju/ygh/dao/JejuDAO5.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Repository;
 public class JejuDAO5 {
 	@Autowired
 	private SqlSessionTemplate sqlSessionTemplate;
-	
+
 	// 관리자 Q&A 페이징
 	public int getTotalCount() {
 		try {
@@ -22,7 +22,7 @@ public class JejuDAO5 {
 		}
 		return -1;
 	}
-	
+
 	// 관리자 Q&A 전체보기
 	public List<BoardVO> boardList(int offset, int limit) {
 		try {
@@ -35,7 +35,7 @@ public class JejuDAO5 {
 		}
 		return null;
 	}
-	
+
 	// 관리자 Q&A 상세보기
 	public BoardVO boardDetail(String bo_idx) {
 		try {
@@ -45,7 +45,7 @@ public class JejuDAO5 {
 		}
 		return null;
 	}
-	
+
 	// 관리자 신고 페이징
 	public int getTotalCount2() {
 		try {
@@ -55,7 +55,7 @@ public class JejuDAO5 {
 		}
 		return -1;
 	}
-	
+
 	// 관리자 신고 전체보기
 	public List<ReportVO> reportList(int offset, int limit) {
 		try {
@@ -68,9 +68,9 @@ public class JejuDAO5 {
 		}
 		return null;
 	}
-	
+
 	// 관리자 신고 상세보기
-	public ReportVO reportDetail(String report_idx){
+	public ReportVO reportDetail(String report_idx) {
 		try {
 			return sqlSessionTemplate.selectOne("Board_table.report_detail", report_idx);
 		} catch (Exception e) {
@@ -78,18 +78,16 @@ public class JejuDAO5 {
 		}
 		return null;
 	}
-	
-	
+
 	public int getLevUpdate(Map<String, Integer> map) {
 		try {
 			return sqlSessionTemplate.update("Board_table.lev_update", map);
 		} catch (Exception e) {
-			System.out.println(e); 
+			System.out.println(e);
 		}
 		return -1;
 	}
 
-	
 	// Q&A 답글 가져오기
 	public List<CommentVO> commentList(String bo_idx) {
 		try {
@@ -99,7 +97,7 @@ public class JejuDAO5 {
 		}
 		return null;
 	}
-	
+
 	// Q&A 답글 작성
 	public int commentInsert(CommentVO comvo) {
 		try {
@@ -109,9 +107,9 @@ public class JejuDAO5 {
 		}
 		return -1;
 	}
-	
+
 	// Q&A 게시판 작성 (사용자)
-	public int boardWrtieOk(BoardVO bovo) {
+	public int boardWriteOk(BoardVO bovo) {
 		try {
 			return sqlSessionTemplate.insert("Board_table.board_insert", bovo);
 		} catch (Exception e) {
@@ -119,7 +117,7 @@ public class JejuDAO5 {
 		}
 		return -1;
 	}
-	
+
 	// Q&A 게시판 수정 (사용자)
 	public int boardUpdate(BoardVO bovo) {
 		try {
@@ -129,6 +127,45 @@ public class JejuDAO5 {
 		}
 		return -1;
 	}
+
+	// Q&A 게시판 삭제 (사용자)
+	public int boardDelete(BoardVO bovo) {
+		try {
+			return sqlSessionTemplate.delete("Board_table.board_delete", bovo);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return -1;
+	}
 	
+	// 신고 게시판 작성 (사용자)
+	public int reportWriteOk(ReportVO revo) {
+		try {
+			return sqlSessionTemplate.insert("Board_table.report_insert", revo);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return -1;
+	}
+	
+	// 신고 게시판 수정 (사용자)
+	public int reportUpdate(ReportVO revo) {
+		try {
+			return sqlSessionTemplate.update("Board_table.report_update", revo);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return -1;
+	}
+	
+	// 신고 게시판 삭제 (사용자)
+	public int reportDelete(ReportVO revo) {
+		try {
+			return sqlSessionTemplate.delete("Board_table.report_delete", revo);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return -1;
+	}
 
 }
